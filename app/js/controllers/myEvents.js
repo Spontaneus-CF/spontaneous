b8fcda5e843362c8347bc8004fb96c6545b24662
Malvin Yeung <malvinyeung@gmail.com>
2014-09-25 15:23:34 -0700

    updates

---
+++ app/js/controllers/myEvents.js
@@ -0,0 +1,72 @@
+"use strict";
+
+module.exports = function(app){
+  app.controller('myEvents', function($scope, $http, auth, $location, $cookies, footer){
+    $scope.getMyEvents = function(){
+      if (auth.sendJWT() === 'noauth') return false;
+      $http({
+        method: 'GET',
+        url: '/api/v_0_0_1/events',
+      })
+      .success(function(data){
+        $scope.events = data;
+        console.log('success');
+      })
+      .error(function(data, status){
+        console.log('error');
+        console.log(data);
+        console.log(status);
+      });
+    };
+    $scope.getMyEvents();
+
+    $scope.userName = $cookies.userName;
+    $scope.getEvents = footer.getEvents;
+    $scope.newEvent = footer.newEvent;
+    $scope.logout = footer.logout;
+
+    $scope.joinEvent = function(event) {
+      var i = event.attendees.indexOf($cookies.firstName);
+      if (i === -1){
+        event.attendees.push($cookies.firstName);
+        $http({
+          method: 'PUT',
+          url:'/api/v_0_0_1/events/' + event._id,
+          data: event
+        })
+        .success(function(data){
+          $location.path('/home'); // change path
+          console.log(data);
+          console.log('success');
+        })
+        .error(function(data, status){
+          console.log('error');
+          console.log(data);
+          console.log(status);
+        });
+      }
+    };
+
+    $scope.unJoinEvent = function(event) { //refactor
+      var i = event.attendees.indexOf($cookies.firstName);
+      if (i !== -1){              // Checks to see if user is in attendees array before executing - maybe change this
+        event.attendees.splice(i, 1);
+        $http({
+          method: 'PUT',
+          url:'/api/v_0_0_1/events/' + event._id,
+          data: event
+        })
+        .success(function(data){
+          $location.path('/home'); // change path
+          console.log(data);
+          console.log('success');
+        })
+        .error(function(data, status){
+          console.log('error');
+          console.log(data);
+          console.log(status);
+        });
+      }
+    };
+  });
+};

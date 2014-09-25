e642596cf8de9015ca574e88675c708a64be82dc
Matt <mattclise@hotmail.com>
2014-09-25 10:39:52 -0700

    Rename file names

---
+++ app/js/controllers/new-event-controller.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = function(app){
+  app.controller('newEventController', function($scope, $http, $location, auth, $cookies, footer){
+    $scope.submitForm = function(){
+      if (auth.sendJWT() === 'noauth') return false;
+      $scope.event.owner = ($cookies.userName);
+      $http({
+        method: 'POST',
+        url: '/api/v_0_0_1/events',
+        data: $scope.event
+      })
+      .success(function(data){
+        $location.path('/home');
+        console.log('success');
+      })
+      .error(function(data){
+        console.log('error');
+        console.log(data);
+      });
+    };
+
+    $scope.getEvents = footer.getEvents;
+    $scope.newEvent = footer.newEvent;
+    $scope.logout = footer.logout;
+  });
+};
